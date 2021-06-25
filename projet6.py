dd747ae52940d7d833555efaa0b9bf8b600d97e3
meme21190 <63185871+meme21190@users.noreply.github.com>
2021-06-25 15:34:07 +0200

    Update projet6.py

--- projet6.py
+++ projet6.py
@@ -13,7 +13,6 @@ os.system('mkdir projet6')
 os.system('mv /home/admin1/sample-webapp/ /home/admin1/projet6/sample-webapp/')
 
 os.system('sudo apt install -y python3-pip')
-os.system('pip3 install rdflib')
 os.system('pip3 install click --user')
 os.system('pip3 install Flask --user')
 os.system('pip3 install gunicorn --user')
